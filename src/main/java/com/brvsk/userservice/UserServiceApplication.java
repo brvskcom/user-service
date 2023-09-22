f4d1f7e03b2064e3e08c574da5e78498de13c002
Kacper <kacpersjuszb@gmail.com>
2023-09-22 11:52:25 +0200

    [new] add admin and manager user

--- src/main/java/com/brvsk/userservice/UserServiceApplication.java
+++ src/main/java/com/brvsk/userservice/UserServiceApplication.java
@@ -1,13 +1,46 @@
 package com.brvsk.userservice;
 
+import com.brvsk.userservice.auth.AuthenticationService;
+import com.brvsk.userservice.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.brvsk.userservice.user.Role.ADMIN;
+import static com.brvsk.userservice.user.Role.MANAGER;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + service.register(manager).getAccessToken());
+
+        };
     }
 
 }
